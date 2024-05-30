07edd0822a02a1fbaeb6b89788274be7d0066794
rinox3005 <peppe.rinox@gmail.com>
2024-05-30 14:57:44 +0200

    [UPD] imported full css and header from old repo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <script>
-export default {};
+export default {
+  name: "App",
+};
 </script>
 
 <template>
-  <header></header>
+  <header>
+    <div class="row container">
+      <div class="row col-33 align-center">
+        <ul class="unstyled-list">
+          <li class="bold">
+            <a href="#">Donna</a>
+          </li>
+          <li class="bold">
+            <a href="#">Uomo</a>
+          </li>
+          <li class="bold">
+            <a href="#">Bambini</a>
+          </li>
+        </ul>
+      </div>
+      <div class="row col-33 justify-center">
+        <a href="#"
+          ><img class="logo" src="/img/boolean-logo.png" alt="logo"
+        /></a>
+      </div>
+      <div class="row col-33 justify-end align-center">
+        <a href="#"
+          ><img class="icons-header" src="/img/user.webp" alt="user"
+        /></a>
+        <a href="#"
+          ><img class="icons-header" src="/img/heart.webp" alt="favourites"
+        /></a>
+        <a href="#"
+          ><img class="icons-header" src="/img/shopping-bag.webp" alt="shop"
+        /></a>
+      </div>
+    </div>
+  </header>
   <main></main>
   <footer></footer>
 </template>
